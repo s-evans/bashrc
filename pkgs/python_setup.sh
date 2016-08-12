eda7152ec1d35f764cd097b1d01af5af4920101d
Sean <evansse1@ITE00402778.northgrum.com>
2016-08-12 18:44:28 -0400

    updated completion; added python package list/script;

---
+++ pkgs/python_setup.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env sh
+
+# pip
+python -m ensurepip
+pip install --upgrade pip
+
+# linters
+pip install mccabe
+pip install mypy
+pip install pep257
+pip install pyflakes
+pip install pylint
+
+# autocomplete
+pip install jedi
+pip install rope
+pip install pybashcomplete
+
+# formatters
+pip install autopep8
+pip install yapf
+
+# other
+pip install virtualenv
+
+# distribution
+pip install cx_freeze
+pip install distribute
+pip install setuptools
+pip install pyzzer
+
+# database
+pip install sqlalchemy
+pip install pymongo
+pip install mongoengine
+
+# web
+pip install pyramid
+pip install waitress
+pip install requests
+pip install requests-futures
+pip install suds
+
+# binary utilities
+pip install binstruct
+pip install bitstruct
+pip install bitarray
+pip install bitstring
+pip install ctypesgen
+
+# test
+pip install nose
+pip install nose-parameterized
+pip install coverage
+pip install injector
+pip install pdb
+
+# network
+pip install scapy
