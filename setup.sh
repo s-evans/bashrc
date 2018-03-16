5f8924d704f3655489ca18d59bf9f5071834dd9d
Sean E <sean!>
2018-03-15 22:42:39 -0400

    setup script: dedup; add paths script; path finding: break out into separate script; dedup;

--- setup.sh
+++ setup.sh
@@ -7,75 +7,78 @@ DIRPATH=`readlink -f $SCRIPTDIR`
 echo "Install directory = ${DIRPATH}"
 
 echo "Creating symbolic link for .bashrc"
-ln -sf ${DIRPATH}/.bashrc ${HOME}/.bashrc
+ln -sf {${DIRPATH},${HOME}}/.bashrc
 
 echo "Creating symbolic link for .bash_functions"
-ln -sf ${DIRPATH}/.bash_functions ${HOME}/.bash_functions
+ln -sf {${DIRPATH},${HOME}}/.bash_functions
 
 echo "Creating symbolic link for .bash_aliases"
-ln -sf ${DIRPATH}/.bash_aliases ${HOME}/.bash_aliases
+ln -sf {${DIRPATH},${HOME}}/.bash_aliases
+
+echo "Creating symbolic link for .bash-paths"
+ln -sf {${DIRPATH},${HOME}}/.bash-paths
 
 echo "Creating symbolic link for .astylerc"
-ln -sf ${DIRPATH}/.astylerc ${HOME}/.astylerc
+ln -sf {${DIRPATH},${HOME}}/.astylerc
 
 echo "Creating symbolic link for .clang-format"
-ln -sf ${DIRPATH}/.clang-format ${HOME}/.clang-format
+ln -sf {${DIRPATH},${HOME}}/.clang-format
 
 echo "Creating symbolic link for .inputrc"
-ln -sf ${DIRPATH}/.inputrc ${HOME}/.inputrc
+ln -sf {${DIRPATH},${HOME}}/.inputrc
 
 echo "Creating symbolic link for .minttyrc"
-ln -sf ${DIRPATH}/.minttyrc ${HOME}/.minttyrc
+ln -sf {${DIRPATH},${HOME}}/.minttyrc
 
 echo "Creating symbolic link for .tmux.conf"
-ln -sf ${DIRPATH}/.tmux.conf ${HOME}/.tmux.conf
+ln -sf {${DIRPATH},${HOME}}/.tmux.conf
 
 echo "Creating symbolic link for .screenrc"
-ln -sf ${DIRPATH}/.screenrc ${HOME}/.screenrc
+ln -sf {${DIRPATH},${HOME}}/.screenrc
 
 echo "Creating symbolic link for .xmltidyrc"
-ln -sf ${DIRPATH}/.xmltidyrc ${HOME}/.xmltidyrc
+ln -sf {${DIRPATH},${HOME}}/.xmltidyrc
 
 echo "Creating symbolic link for .htmltidyrc"
-ln -sf ${DIRPATH}/.htmltidyrc ${HOME}/.htmltidyrc
+ln -sf {${DIRPATH},${HOME}}/.htmltidyrc
 
 echo "Creating symbolic link for .cmakelintrc"
-ln -sf ${DIRPATH}/.cmakelintrc ${HOME}/.cmakelintrc
+ln -sf {${DIRPATH},${HOME}}/.cmakelintrc
 
 echo "Creating symbolic link for .infokey"
-ln -sf ${DIRPATH}/.infokey ${HOME}/.infokey
+ln -sf {${DIRPATH},${HOME}}/.infokey
 
 echo "Creating symbolic link for .lesskey"
-ln -sf ${DIRPATH}/.lesskey ${HOME}/.lesskey
+ln -sf {${DIRPATH},${HOME}}/.lesskey
 
 echo "Compiling lesskey to create mappings for less"
 lesskey
 
 echo "Creating symbolic link for .bash_profile"
-ln -sf ${DIRPATH}/.bash_profile ${HOME}/.bash_profile
+ln -sf {${DIRPATH},${HOME}}/.bash_profile
 
 echo "Creating symbolic link for gnome-terminal config"
 mkdir -p ${HOME}/.gconf/apps/gnome-terminal/profiles/Default/
-ln -sf ${DIRPATH}/%gconf.xml ${HOME}/.gconf/apps/gnome-terminal/profiles/Default
+ln -sf {${DIRPATH},${HOME}/.gconf/apps/gnome-terminal/profiles/Default}/%gconf.xml
 
 echo "Creating symbolic link for xterm config"
-ln -sf ${DIRPATH}/.Xresources ${HOME}/.Xresources
+ln -sf {${DIRPATH},${HOME}}/.Xresources
 ln -sf ${DIRPATH}/.Xresources ${HOME}/.Xdefaults
 
 echo "Creating symbolic link for .gitignore_global"
-ln -sf ${DIRPATH}/.gitignore_global ${HOME}/.gitignore_global
+ln -sf {${DIRPATH},${HOME}}/.gitignore_global
 
 echo "Creating symbolic link for .gitconfig_global"
-ln -sf ${DIRPATH}/.gitconfig_global ${HOME}/.gitconfig_global
+ln -sf {${DIRPATH},${HOME}}/.gitconfig_global
 
 echo "Copying .gitconfig"
-cp -n ${DIRPATH}/.gitconfig ${HOME}/.gitconfig
+ln -sf {${DIRPATH},${HOME}}/.gitconfig
 
 echo "Updating .gitconfig to include .gitconfig_global"
 git config --global include.path ${HOME}/.gitconfig_global
 
 echo "Copying .bash_local"
-cp -n ${DIRPATH}/.bash_local ${HOME}/.bash_local
+cp -n {${DIRPATH},${HOME}}/.bash_local
 
 echo "Reloading inputrc"
 bind -f ${HOME}/.inputrc
